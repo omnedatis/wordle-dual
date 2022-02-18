d8383b43ae3a03fcdede0328dda66b93f4b9b52c
JeffYeh <jeffcyyeh@gmail.com>
2022-02-18 22:01:54 +0800

    feat: change environment building method

--- setup.bat
+++ setup.bat
@@ -1,6 +1,7 @@
 cd web
-start npm run dev
+call npm install --save
 cd ../wordle_genie
 call C:\Users\qoa62\miniconda3\Scripts\activate.bat
+call conda create --name wordle_genie python=3.8 -y
 call conda activate wordle_genie
-python genie.py
+pip install -r requirements.txt
